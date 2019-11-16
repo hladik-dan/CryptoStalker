e836d13006d92ef06868ff7c591357cf415d8435
Daniel Hladík <dhladik1993@gmail.com>
2019-11-16 17:00:22 +0100

    Initial Commit

---
+++ CryptoStalker/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  CryptoStalker
+//
+//  Created by Daniel Hladík on 16/11/2019.
+//  Copyright © 2019 Daniel Hladík. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
